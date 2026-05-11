174824d6e16f94aefa133a7710b7e9875f67768c
Joseph Platta <jwplatta@gmail.com>
2026-05-11 15:54:43 -0500

    remove

--- experiments/sp500-event-driven/v3_intraday_exhaustion_reversal.py
+++
@@ -1,8 +0,0 @@
-from common import build_intraday_exhaustion_study, emit_metrics, save_study
-
-STUDY_NAME = "sp500_event_driven_v3_intraday_exhaustion_reversal"
-study = build_intraday_exhaustion_study(STUDY_NAME)
-
-if __name__ == "__main__":
-    save_study(study, STUDY_NAME)
-    emit_metrics(study)
